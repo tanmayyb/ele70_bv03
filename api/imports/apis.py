f70cfbcb5004ce4c8b386ac6b385056290b08e62
tanmayyb <tbishnoi@torontomu.ca>
2024-10-31 16:34:15 -0400

    add first climate api

--- api/imports/apis.py
+++ api/imports/apis.py
@@ -7,7 +7,7 @@
 
 import pandas as pd
 
-# Dataloader
+# Dataloader for Energy Demand (IESO Zonal Ontario-wide)
 def load_ieso_dataset( first_year:int, last_year:int, join:bool=False) -> dict|pd.DataFrame:
   # loads dataset from multiple years
 
@@ -48,3 +48,46 @@ def load_ieso_dataset( first_year:int, last_year:int, join:bool=False) -> dict|p
 
     return dataset
 
+# Dataloader for Weather (Canada-Wide)
+def load_climate_dataset(first_year:int, last_year:int, station_id:int=31688, join:bool=False) -> dict|pd.DataFrame:
+
+  # assertion checks
+  assert first_year <= last_year, "invalid entry for first/last year"
+  assert first_year>=2003, "invalid entry for first_year, please select >=2003"
+  assert last_year<=2024, "invalid entry for last year"
+
+  # hourly data for a given year, and station
+  # f'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&time=UTC&stationID=31688&Year=2008&Month={i}&timeframe=1&submit=Download+Data'
+
+  num_years = last_year - first_year
+
+  url_prefix = 'https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv'
+  timeframe = 1
+
+  if num_years<=0:
+    tmp = []
+    for month in range(1, 12+1):
+      url = url_prefix+f'&time=UTC&stationID={station_id}&Year={first_year}&Month={month}&timeframe={timeframe}&submit=Data'
+      tmp.append(pd.read_csv(url))
+    df = pd.concat(tmp)
+    return df
+
+  else:
+    # data = dict()
+    if not join:
+      data = dict()
+    else:
+      data = pd.DataFrame()
+
+    for year in tqdm(range(first_year, last_year+1)):
+      tmp = []
+      for month in range(1, 12+1):
+        url = url_prefix+f'&time=UTC&stationID={station_id}&Year={year}&Month={month}&timeframe={timeframe}&submit=Data'
+        tmp.append(pd.read_csv(url))
+      tmp = pd.concat(tmp)
+      
+      if not join:
+        data[year] = tmp
+      else:
+        data = pd.concat([data, tmp])
+    return data
