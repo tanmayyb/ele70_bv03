6502daff9075c9cd77145e019b663d4a2830abe8
Praarora4 <prateek.arora@torontomu.ca>
2025-03-18 20:37:42 -0400

    updated to be class-based

--- research/anomaly_detection/anomaly_detection.py
+++ research/anomaly_detection/anomaly_detection.py
@@ -5,149 +5,211 @@ from sklearn.mixture import GaussianMixture
 from sklearn.cluster import DBSCAN
 import seaborn as sns
 from scipy.stats import norm
+from prettytable import PrettyTable
 
-def anomaly_detection(csv_data, target):
+class AnomalyDetection:
 
-  def statistical_detection(csv_data, num_stds):
-    actual = csv_data['Toronto']
-    predicted = csv_data['pred']
-    errors = csv_data['error']
-    data = pd.DataFrame({'Predicted': predicted, 'Actual': actual})
+  def __init__(self, csv_data, target):
+    self.csv_data = csv_data
+    self.data = pd.DataFrame({'DateTime': self.csv_data['DateTime'], 'Actual': self.csv_data['Toronto'], 'Predicted': self.csv_data['pred']})
+    self.data['DateTime'] = pd.to_datetime(self.data['DateTime'])
+    self.target = target
+    self.errors = self.csv_data['error']
+    self.actual = self.csv_data[self.target]
+    self.predicted = self.csv_data['pred']
+    self.anomalies = pd.DataFrame()
+    self.statistical_detection()
+    self.gmm()
 
-    mean = errors.mean()
-    std = errors.std()
+  def statistical_detection(self):
+    num_stds = 2
+
+    mean = self.errors.mean()
+    std = self.errors.std()
     upper_threshold = mean + num_stds * std
     lower_threshold = mean - num_stds * std
 
-    anomalies = data[(errors > upper_threshold) | (errors < lower_threshold)]
+    self.anomalies = self.data[(self.errors > upper_threshold) | (self.errors < lower_threshold)]
 
-    plt.figure(figsize=(10, 6))
-    plt.scatter(predicted, actual, label='Normal data', c='blue', alpha=0.7)
-    plt.scatter(anomalies['Predicted'], anomalies['Actual'], label='Anomalies', c='red', edgecolors='k')
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.title(f'Statistical anomaly detection with num_stds={num_stds}')
-    plt.legend()
-    plt.show()
+    #plt.figure(figsize=(10, 6))
+    #plt.scatter(self. predicted, self.actual, label='Normal data', c='blue', alpha=0.7)
+    #plt.scatter(self.anomalies['Predicted'], self.anomalies['Actual'], label='Anomalies', c='red', edgecolors='k')
+    #plt.xlabel('Predicted')
+    #plt.ylabel('Actual')
+    #plt.title(f'Statistical anomaly detection with num_stds={num_stds}')
+    #plt.legend()
+    #plt.show()
 
-    return anomalies
-
-  def gmm(csv_data, percentile, prev_anomalies):
-
-    actual = csv_data['Toronto']
-    predicted = csv_data['pred']
-    errors = csv_data['error'].values.reshape(-1, 1)
+  def gmm(self, ax=None):
+    errors = self.errors.values.reshape(-1, 1)
 
     gmm = GaussianMixture(n_components=1, random_state=42).fit(errors)
 
     log_likelihood = gmm.score_samples(errors)      # how well the model explains observed data
-    threshold = np.percentile(log_likelihood, percentile)
-    data = pd.DataFrame({'Predicted': predicted, 'Actual': actual})
-    anomalies = data[log_likelihood < threshold]
+    threshold = np.percentile(log_likelihood, 5)
+    gmm_anomalies = self.data[log_likelihood < threshold]
 
-    common_index = anomalies.index.intersection(prev_anomalies.index)
-    anomalies = anomalies.loc[common_index]
+    common_index = gmm_anomalies.index.intersection(self.anomalies.index)
+    self.anomalies = gmm_anomalies.loc[common_index]
 
-    p_vals = np.exp(log_likelihood[anomalies.index])
+    p_vals = np.exp(log_likelihood[self.anomalies.index])
     delta_x = (3.5*errors.std())/(len(errors)**(1/3))
     approx_p = p_vals * delta_x
     norm_scores = 1 - (approx_p - approx_p.min()) / (approx_p.max() - approx_p.min())
-    anomalies['Anomaly Score'] = norm_scores
+    self.anomalies['Anomaly Score'] = norm_scores
 
-    plt.figure(figsize=(10, 6))
-    plt.scatter(predicted, actual, label='Normal data', c='blue', alpha=0.7)
-    #plt.scatter(anomalies['Predicted'], anomalies['Actual'], label='Anomalies', c='red', edgecolors='k')
-    sc = plt.scatter(anomalies['Predicted'], anomalies['Actual'], c=norm_scores, cmap='viridis', edgecolors='k', s=50, label='Anomalies')
-    cbar = plt.colorbar(sc)
+  # plots scatter plot of anomalies and normal data points
+  def final_plot(self, ax=None):
+    if ax is None:
+        plt.figure(figsize=(10, 6))
+        ax = plt.gca()
+
+    ax.scatter(self.predicted, self.actual, label='Normal data', c='blue', alpha=0.7)
+    sc = ax.scatter(self.anomalies['Predicted'], self.anomalies['Actual'], c=self.anomalies['Anomaly Score'], cmap='viridis', edgecolors='k', s=50, label='Anomalies')
+
+    cbar = plt.colorbar(sc, ax=ax)
     cbar.set_label('Anomaly Likelihood')
 
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.title('Final Anomalies')
-    plt.legend()
+    ax.set_xlabel('Predicted')
+    ax.set_ylabel('Actual')
+    ax.set_title('Final Anomalies')
+    ax.legend()
+
+  # prints number of anomalies detected
+  def num_anomalies(self):
+    print(f'\n{len(self.anomalies)} final anomalies detected out of {len(self.csv_data)} data points using Gaussian mixture modelling and statistical calculations.\n')
+
+  # prints table of best ten anomalies (highest anomaly scores)
+  def best_ten_anomalies(self):
+    table = PrettyTable(["DateTime", "Toronto", "Predicted", "Anomaly Score"])
+    best_ten = self.anomalies.sort_values(by=['Anomaly Score'], ascending=True)[0:10]
+    table.title = "Best ten anomalies"
+    for index, row in best_ten.iterrows():
+      table.add_row([row['DateTime'], row['Actual'], row['Predicted'], row['Anomaly Score']])
+    print(table)
+
+  # prints table of worst ten anomalies (lowest anomaly scores)
+  def worst_ten_anomalies(self):
+    table = PrettyTable(["DateTime", "Toronto", "Predicted", "Anomaly Score"])
+    worst_ten = self.anomalies.sort_values(by=['Anomaly Score'], ascending=False)[0:10]
+    table.title = "Worst ten anomalies"
+    for index, row in worst_ten.iterrows():
+      table.add_row([row['DateTime'], row['Actual'], row['Predicted'], row['Anomaly Score']])
+    print(table)
+
+  # plots longest streak of consecutive anomalies 
+  def longest_anomalous_streak(self, ax=None):
+    self.anomalies['group'] = (self.anomalies.index.to_series().diff() != 1).cumsum()
+    longest_group = self.anomalies['group'].value_counts().idxmax()
+    longest_scores = self.anomalies[self.anomalies['group'] == longest_group]
+    longest_scores_idx = longest_scores.index
+    consecutive_anomalies = self.anomalies.loc[longest_scores_idx]
+    hours = consecutive_anomalies.index - consecutive_anomalies.index[0]
+    consecutive_anomalies['Hour'] = hours
+
+    if ax is None:
+        plt.figure(figsize=(10, 6))
+        ax = plt.gca()
+
+    ax.plot(consecutive_anomalies['DateTime'], consecutive_anomalies['Actual'], label='Actual')
+    ax.plot(consecutive_anomalies['DateTime'], consecutive_anomalies['Predicted'], label='Predicted')
+
+    xticks = np.arange(consecutive_anomalies.index[0], consecutive_anomalies.index[len(consecutive_anomalies)-1], 2)
+    ax.set_xticks(consecutive_anomalies['DateTime'][xticks])
+    ax.tick_params(axis='x', rotation=45)
+
+    ax.set_xlabel('Hour')
+    ax.set_ylabel('Power')
+    ax.set_title(f"Most consecutive anomalies on {consecutive_anomalies['DateTime'][consecutive_anomalies.index[0]]}")
+    ax.legend()
+
+  # plots longest streak of consecutive nonanomalous points
+  def longest_nonanomalous_streak(self, ax=None):
+    differences = self.anomalies.index.to_series().diff()
+    max_diff_idx = int(differences.idxmax())
+    prev_idx = int(max_diff_idx - differences.loc[max_diff_idx])
+    total_days = (max_diff_idx - prev_idx) / 24
+    tick_interval = 48 if total_days > 7 else 24
+
+    if ax is None:
+        plt.figure(figsize=(10, 6))
+        ax = plt.gca()
+
+    ax.plot(self.data['DateTime'][prev_idx:max_diff_idx+1], self.data['Actual'][prev_idx:max_diff_idx+1], label='Actual')
+    ax.plot(self.data['DateTime'][prev_idx:max_diff_idx+1], self.data['Predicted'][prev_idx:max_diff_idx+1], label='Predicted')
+
+    ax.set_xlabel('DateTime')
+    ax.set_ylabel('Power')
+    ax.set_title(f"Longest period without an anomaly: {self.data.loc[prev_idx]['DateTime']} to {self.data.loc[max_diff_idx]['DateTime']}")
+
+    xticks = np.arange(prev_idx, max_diff_idx + 1, tick_interval)
+    ax.set_xticks(self.data['DateTime'][xticks])
+    ax.tick_params(axis='x', rotation=45)
+
+    ax.legend()
+
+  # plots anomaly frequency per month
+  def anomalies_per_month(self, ax=None):
+    anomaly_counts = self.anomalies['DateTime'].dt.month.value_counts().sort_index()
+    total_counts = self.data['DateTime'].dt.month.value_counts().sort_index()
+
+    normalized_counts = anomaly_counts / total_counts
+
+
+    if ax is None:
+        plt.figure(figsize=(10, 6))
+        ax = plt.gca()
+
+    ax.bar(normalized_counts.index, normalized_counts.values)
+    ax.set_xlabel('Month')
+    ax.set_ylabel('Proportion of anomalies')
+    ax.set_title('Anomaly Frequency by Month (normalized)')
+    ax.set_xticks(range(1, 13))
+    ax.set_ylim(0, normalized_counts.max() + 0.05)
+
+  # plots anomaly frequency per day
+  def anomalies_per_day(self):
+    anomaly_counts = self.anomalies['DateTime'].dt.day.value_counts().sort_index()
+    total_counts = self.data['DateTime'].dt.day.value_counts().sort_index()
+    normalized_counts = anomaly_counts / total_counts
+    plt.figure(figsize=(10, 6))
+    plt.bar(normalized_counts.index, normalized_counts.values)
+    plt.xlabel('Day')
+    plt.ylabel('Proportion of anomalies')
+    plt.title('Anomaly Frequency by Day (normalized)')
+    plt.xticks(range(1, 32))  # Ensure days are labeled 1 to 31
+    plt.ylim(0, normalized_counts.max() + 0.05)  # Since it's a proportion
     plt.show()
 
-    return anomalies
+  # plots anomaly frequency per hour
+  def anomalies_per_hour(self):
+    anomaly_counts = self.anomalies['DateTime'].dt.hour.value_counts().sort_index()
+    total_counts = self.data['DateTime'].dt.hour.value_counts().sort_index()
+    normalized_counts = anomaly_counts / total_counts
+    plt.figure(figsize=(10, 6))
+    plt.bar(normalized_counts.index, normalized_counts.values)
+    plt.xlabel('Hour')
+    plt.ylabel('Proportion of anomalies')
+    plt.title('Anomaly Frequency by Hour (normalized)')
+    plt.xticks(range(24))
+    plt.ylim(0, normalized_counts.max() + 0.05)
+    plt.show()
 
-  display(csv_data)
+  # plots four main plots
+  def summary_plots(self):
+    fig, axes = plt.subplots(2, 2, figsize=(18, 15))
 
-  statistical_anomalies = statistical_detection(csv_data, 2)    # approx. 95% of data lies within 2 standard deviations
-  print(f'{len(statistical_anomalies)} anomalies detected out of {len(csv_data)} using statistical calculations')
+    # scatter plot
+    self.final_plot(ax=axes[0, 0])
 
-  gmm_anomalies= gmm(csv_data, 5, statistical_anomalies)
-  print(f'{len(gmm_anomalies)} final anomalies detected out of {len(csv_data)} using GMM and statistical calculations')
-  csv_data['Anomaly Score'] = gmm_anomalies['Anomaly Score']
-  worst_ten_idx = gmm_anomalies.sort_values(by=['Anomaly Score'], ascending=False)[0:10].index
-  best_ten_idx = gmm_anomalies.sort_values(by=['Anomaly Score'], ascending=True)[0:10].index
-  worst_ten_anomalies = csv_data.loc[worst_ten_idx]
-  best_ten_anomalies = csv_data.loc[best_ten_idx]
-  print('\nWorst ten anomalies')
-  display(worst_ten_anomalies[['DateTime', 'Toronto', 'pred', 'Anomaly Score', ]])
+    # anomalies per month
+    self.anomalies_per_month(ax=axes[0, 1])
 
-  print('\nBest ten anomalies')
-  display(best_ten_anomalies[['DateTime', 'Toronto', 'pred', 'Anomaly Score']])
+    # longest anomalous streak
+    self.longest_anomalous_streak(ax=axes[1, 0])
 
-  # find longest streak of consecutive anomalies
-  gmm_anomalies['group'] = (gmm_anomalies.index.to_series().diff() != 1).cumsum()
-  longest_group = gmm_anomalies['group'].value_counts().idxmax()
-  longest_scores = gmm_anomalies[gmm_anomalies['group'] == longest_group]
-  longest_scores_idx = longest_scores.index
-  consecutive_anomalies = csv_data.loc[longest_scores_idx]
-  hours = consecutive_anomalies.index - consecutive_anomalies.index[0]
-  consecutive_anomalies['Hour'] = hours
+    # longest non-anomalous streak
+    self.longest_nonanomalous_streak(ax=axes[1, 1])
 
-  highest_score_in_streak = consecutive_anomalies.max()['Anomaly Score']
-  actual_max = consecutive_anomalies[consecutive_anomalies['Anomaly Score'] == highest_score_in_streak]['Toronto']
-  pred_max = consecutive_anomalies[consecutive_anomalies['Anomaly Score'] == highest_score_in_streak]['pred']
-  hour_max = consecutive_anomalies[consecutive_anomalies['Anomaly Score'] == highest_score_in_streak]['Hour']
-  lowest_score_in_streak= consecutive_anomalies.min()['Anomaly Score']
-  actual_min = consecutive_anomalies[consecutive_anomalies['Anomaly Score'] == lowest_score_in_streak]['Toronto']
-  pred_min = consecutive_anomalies[consecutive_anomalies['Anomaly Score'] == lowest_score_in_streak]['pred']
-  hour_min = consecutive_anomalies[consecutive_anomalies['Anomaly Score'] == lowest_score_in_streak]['Hour']
-
-  plt.figure(figsize=(10, 6))
-  plt.plot(hours, consecutive_anomalies['Toronto'], label='Toronto')
-  plt.plot(hours, consecutive_anomalies['pred'], label='Predicted')
-  plt.plot(hour_max, actual_max, 'bo')
-  plt.plot(hour_max, pred_max, 'bo', label='Highest Anomaly Score')
-  plt.plot(hour_min, actual_min, 'ro')
-  plt.plot(hour_min, pred_min, 'ro', label='Lowest Anomaly Score')
-  plt.xticks(consecutive_anomalies['Hour'].unique())
-  plt.xlabel('Hour')
-  plt.ylabel('Power')
-  plt.title(f'Most consecutive anomalies on {consecutive_anomalies["DateTime"][longest_scores_idx[0]][0:10]} from {consecutive_anomalies["DateTime"][longest_scores_idx[0]][11::]}')
-  plt.legend()
-  plt.show()
-
-  differences = gmm_anomalies.index.to_series().diff()
-  max_diff_idx = int(differences.idxmax())
-  prev_idx = int(max_diff_idx - differences.loc[max_diff_idx])
-
-  print('\nLongest period without an anomaly:')
-  print(f"From {csv_data.loc[prev_idx]['DateTime']} to {csv_data.loc[max_diff_idx]['DateTime']}")
-
-  plt.figure(figsize=(10, 6))
-  plt.plot(csv_data.index.to_series()[prev_idx:max_diff_idx+1], csv_data['Toronto'][prev_idx:max_diff_idx+1], label='Actual')
-  plt.plot(csv_data.index.to_series()[prev_idx:max_diff_idx+1], csv_data['pred'][prev_idx:max_diff_idx+1], label='Predicted')
-  plt.xlabel('Index')
-  days = pd.to_datetime(csv_data['DateTime'][prev_idx:max_diff_idx]).dt.day
-  plt.ylabel('Power')
-  plt.title(f"Longest period without an anomaly: {csv_data.loc[prev_idx]['DateTime']} to {csv_data.loc[max_diff_idx]['DateTime']}")
-  plt.legend()
-  plt.show()
-
-  anomaly_data = csv_data[csv_data['Anomaly Score'] >= 0]
-  month_counts = pd.to_datetime(anomaly_data['DateTime']).dt.month.value_counts()
-  plt.figure(figsize=(10, 6))
-  plt.bar(month_counts.index, month_counts.values)
-  plt.xlabel('Month')
-  plt.ylabel('Number of anomalies')
-  plt.title('Number of anomalies per month')
-  plt.xticks(range(1, 13))
-  plt.show()
-
-#csv_data = pd.read_csv('/content/drive/MyDrive/mcr4_xgb_mt1r1_pred_eval.csv')
-#anomaly_detection(csv_data, 'Toronto')
-
-#csv_data = pd.read_csv('/content/lstm_Toronto_2.csv')
-#anomaly_detection(csv_data, 'Y_test')
+    plt.tight_layout()
+    plt.show()
